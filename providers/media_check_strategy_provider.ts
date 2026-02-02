c17e316994e51e66aeef7fa6364dd41ec60b9db9
WilFriite <jnguessan2003@gmail.com>
2026-02-02 17:49:35 +0100

    Add dynamic import

--- providers/media_check_strategy_provider.ts
+++ providers/media_check_strategy_provider.ts
@@ -1,8 +1,6 @@
 import type { ApplicationService } from "@adonisjs/core/types";
 import User from "#models/user";
 import { JellyfinService } from "#services/jellyfin_service";
-import { EveryoneMustSeeStrategy } from "#services/media_check/strategies/everyone_must_see_strategy";
-import { OnlyRequesterMustSeeStrategy } from "#services/media_check/strategies/only_requester_must_see_strategy";
 import { MediaCheckStrategy } from "#services/media_check/strategies/types";
 import { MediaRequestService } from "#services/media_request_service";
 import env from "#start/env";
@@ -11,16 +9,21 @@ export default class MediaCheckStrategyProvider {
   constructor(protected app: ApplicationService) {}
 
   public async boot() {
-    // On enregistre notre service dans le container
     this.app.container.bind(MediaCheckStrategy, async (resolver) => {
       const strategyType = env.get("MEDIA_CHECK_STRATEGY", "requester");
       const jellyfinService = await resolver.make(JellyfinService);
       const users = await User.all();
 
       if (strategyType === "everyone") {
+        const { EveryoneMustSeeStrategy } = await import(
+          "#services/media_check/strategies/everyone_must_see_strategy"
+        );
         return new EveryoneMustSeeStrategy(jellyfinService, users);
       }
 
+      const { OnlyRequesterMustSeeStrategy } = await import(
+        "#services/media_check/strategies/only_requester_must_see_strategy"
+      );
       // Par défaut : Requester strategy
       const requestService = await resolver.make(MediaRequestService);
       const requests = await requestService.getAllRequests();
